01dd740d7c7c978afe2813630ef24b574c4dd9de
Cristianyelmo <cristianama2019@gmail.com>
2023-09-03 13:52:02 -0300

    change 3d to img

--- src/components/Tech/index.jsx
+++ src/components/Tech/index.jsx
@@ -15,7 +15,7 @@ const Tech = ()=>{
             <div >
               
         <div className="w-[150px] h-[150px] ">
-              <EarthCanvas2 />
+             <img  src={tech.url}  />
                
             </div>
             <p className="text-center text-white">{tech.name}</p>
